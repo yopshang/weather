b0f693182742adc5fef63756a5eac3b2d1161240
EZPAY_OA\shang.huang <shang.huang@ezpay.com.tw>
2022-03-04 11:16:42 +0800

    建立專案

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <mainNav></mainNav>
+  </div>
+</template>
+
+<script>
+import mainNav from './components/nav.vue'
+export default {
+  name: 'App',
+  components: {
+    mainNav 
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
