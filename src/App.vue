a6b44752b7de267ccb2bfe33b704d76f240b3bf2
Shang <yopshang@gmail.com>
2022-03-18 21:38:28 +0800

    新增：搜尋該縣市

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
           <twMap @get_active="get_active"></twMap >
         </div>
         <div class="col-4">
-          <h1>{{country}}</h1>
+          <h1>{{title}}</h1>
+          <h2>{{country_name}}</h2>
         </div>
       </div>
 
@@ -26,21 +27,50 @@ export default {
   },
   data(){
     return {
-      country: ''
+      country_name: '',
+      title: '',
+      countries:[]
     }
   },
   methods: {
     get_active(country){
-      this.country = country;
+      this.country_name = country;
+      this.get_active_data_of_country(country);
     },
     get_wather_data(){
-      var token = 'CWB-515CB401-8DE7-4F50-A18F-477C0E6DB913';
       var api = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization=CWB-515CB401-8DE7-4F50-A18F-477C0E6DB913';
       this.axios.get(api)
         .then(res => {
-          console.log(res.data)
+          var weather_data = res.data;
+          if(res.data.success){
+            this.country_weather_handler(weather_data.records.location);
+            this.country_title_handler(weather_data.records.datasetDescription);
+          }else {
+            alert('網路連線錯誤');
+            window.location.reload();
+          }
         });
+    },
+    country_weather_handler(countries){
+        this.countries = countries;
+    },
+    country_title_handler(title){
+      this.title = title;
+    },
+    get_active_data_of_country(country){
+      // var country_name = this.country_name;
+      var countries = this.countries;
+      countries.forEach(function(item, i){
+        // console.log(of_this_country);
+        if(item.locationName == country){
+          console.log('country:', country);
+        }
+      })
+    },
+    active(weatherElement){
+
     }
+
   },
   created(){
     this.get_wather_data();
