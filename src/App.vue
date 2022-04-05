04fd53587de5cd44fc8c211d5aa75df45704a1df
Shang <yopshang@gmail.com>
2022-04-05 11:04:08 +0800

    修正router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <!-- <nav>
+    <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
-  <router-view/> -->
+  <router-view/>
     <mainNav></mainNav>
     <div class="container">
       <div class="row">
